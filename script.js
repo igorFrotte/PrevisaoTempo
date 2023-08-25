798bac8c613763103da0fd1d12ecefd67c82c600
igorFrotte <igorfrotte@gmail.com>
2023-08-25 13:24:21 -0300

    add shadow

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 
 //id: "lat_long"
 let cityHash = {
-    path2331: "oi", //Cuiabá
-    path2332: "oi", //Rio de Janeiro
-    path2333: "oi", //São Paulo
-    path2334: "oi", //Manaus
-    path2335: "oi", //São Luís
-    path2336: "oi", //Aracaju
-    path2337: "oi", //Salvador
-    path2338: "oi", //Maceió
-    path2339: "oi", //Rio Branco
-    path2340: "oi", //Recife
-    path2341: "oi", //Natal
-    path2342: "oi", //Curitiba
-    path2343: "oi", //Boa Vista
-    path2344: "oi", //Campo Grande
-    path2345: "oi", //Belo Horizonte
-    path2346: "oi", //Porto Alegre
-    path2347: "oi", //Palmas
-    path2351: "oi", //Porto Velho
-    path2355: "oi", //Teresina
-    path2359: "oi", //Fortaleza
-    path2363: "oi", //Belém
-    path2371: "oi", //Goiânia
-    path2375: "oi", //Macapá
-    path2379: "oi", //Florianópolis
-    path2389: "oi", //João Pessoa
-    path2401: "oi", //Vitória
+    2331: "oi", //Cuiabá
+    2332: "oi", //Rio de Janeiro
+    2333: "oi", //São Paulo
+    2334: "oi", //Manaus
+    2335: "oi", //São Luís
+    2336: "oi", //Aracaju
+    2337: "oi", //Salvador
+    2338: "oi", //Maceió
+    2339: "oi", //Rio Branco
+    2340: "oi", //Recife
+    2341: "oi", //Natal
+    2342: "oi", //Curitiba
+    2343: "oi", //Boa Vista
+    2344: "oi", //Campo Grande
+    2345: "oi", //Belo Horizonte
+    2346: "oi", //Porto Alegre
+    2347: "oi", //Palmas
+    2348: "oi", //Vitória
+    2349: "oi", //João Pessoa
+    2350: "oi", //Florianópolis  
+    2351: "oi", //Porto Velho
+    2352: "oi", //Macapá  
+    2353: "oi", //Goiânia
+    2354: "oi", //Belém
+    2355: "oi", //Teresina
+    2356: "oi", //Fortaleza
 };
 
 let ids = Object.keys(cityHash);
 
 ids.map((e) => {
-    document.querySelector(`#${e}`).onclick = function() {
+    let element = document.querySelector(`#path${e}`);
+    element.onclick = function() {
         alert(cityHash[e]);
     };
-    document.querySelector(`#${e}`).classList.add("cap");
+    element.classList.add("cap");
+    element.addEventListener('mouseover', (event) => {
+        document.querySelector(`#text${e}`).classList.toggle("shadow");
+    });
+    element.addEventListener('mouseout', (event) => {
+        document.querySelector(`#text${e}`).classList.toggle("shadow");
+    });
 });
